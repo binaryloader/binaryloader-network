f748a91548bafd72bebcfff71ead3fb8c54a31db
동욱 <152517454+binaryloader@users.noreply.github.com>
2026-02-15 02:16:30 +0900

    feat: add xcodeproj and supporting files to match other modules

---
+++ BinaryloaderNetwork/Module/Supporting Files/BinaryloaderNetwork.h
@@ -0,0 +1,17 @@
+//
+//  BinaryloaderNetwork.h
+//  BinaryloaderNetwork
+//
+//  Created by binaryloader on 2022/08/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinaryloaderNetwork.
+FOUNDATION_EXPORT double BinaryloaderNetworkVersionNumber;
+
+//! Project version string for BinaryloaderNetwork.
+FOUNDATION_EXPORT const unsigned char BinaryloaderNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinaryloaderNetwork/PublicHeader.h>
+
